b7649783e5c5a390d6b07c4d0c3fa5bebbadf17b
Ziyad Alvi <ziyadalvi@outlook.com>
2017-06-24 16:08:55 +0500

    Objects

--- Objects/3basicformatting.py
+++
@@ -1,8 +0,0 @@
-#Basic formatting
-#Strings also support an advanced
-#substitution operation known as formatting, available as both an #expression (the orignal) and a string method call
-
-print('%s, eggs, and %s' % ('paratha', 'CHEESE!')) #formatting expression
-print('{0}, eggs, and {1}'.format('paratha','CHEESE!')) #formatting method
-print('{}, eggs, and {}'.format('paratha','CHEESE!')) #numbers optional
-
